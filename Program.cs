99e8ac3703475f19b7c574de4e96c6f9356599de
Juan F Luna <105814988+JuanFLuna@users.noreply.github.com>
2022-05-27 10:14:37 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_Elevar_al_cuadrado
+{
+    class Program
+    {
+        class cuadrado
+        {
+            public double Num;
+            public double Res;
+            public cuadrado(double Num)
+            {
+                this.Num = Num;
+            }
+            public double calcular()
+            {
+                return this.Res = Math.Pow(Num, 2);
+            }
+            public void resultado()
+            {
+                Console.WriteLine("Elevado al cuadrado es: " + this.Res);
+            }
+            ~cuadrado()
+            {
+                Console.WriteLine("Memoria liberada cuadrado");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingrese un numero o una cifra: ");
+                double Num = double.Parse(Console.ReadLine());
+                cuadrado e1 = new cuadrado(Num);
+
+                Console.Clear();
+
+
+                e1.calcular();
+                e1.resultado();
+                Console.WriteLine("Oprima ENTER para continuar");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Ha ocurrido un error " + error.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
